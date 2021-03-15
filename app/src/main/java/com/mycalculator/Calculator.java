6eda373ab2180f6d980d03325fd8080fb4ba6673
Hammad Kayani <36533976+kayani7860@users.noreply.github.com>
2021-03-15 22:03:46 +0100

    Update Calculator.java

--- app/src/main/java/com/mycalculator/Calculator.java
+++ app/src/main/java/com/mycalculator/Calculator.java
@@ -18,7 +18,7 @@ public class Calculator {
     }
 
     public static double multiply(double x, double y) {
-        return x + y;
+        return x * y;
     }
 
     public static int multiply(int x, int y) {
